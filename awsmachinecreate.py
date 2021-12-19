3a05448821c946e628eeb0381d2ce0a1d05efee1
Killer2171 <66002179+Killer2171@users.noreply.github.com>
2021-12-19 18:16:44 +0200

    Update awsmachinecreate.py

--- awsmachinecreate.py
+++ awsmachinecreate.py
@@ -17,7 +17,7 @@ print(get_instance_ids(client))
 ec2 = boto3.resource('ec2') 
    
 def create_instance(ec2):
-response = ec2.create_instances(SubnetId="subnet-06cfb6d887ac1cae8", InstanceType="t2.micro", ImageId="ami-0ed9277fb7eb570c9", MaxCount=1, MinCount=1)
-return response
+    response = ec2.create_instances(SubnetId="subnet-06cfb6d887ac1cae8", InstanceType="t2.micro", ImageId="ami-0ed9277fb7eb570c9", MaxCount=1, MinCount=1)
+    return response
     
 print(create_instance(ec2))
